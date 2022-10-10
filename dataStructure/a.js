63dd9bc1a86b18e9ce1f39288bba808cae38b923
xujie <1607526161@qq.com>
2022-10-10 20:57:22 +0800

    1

---
+++ dataStructure/a.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: xujie 1607526161@qq.com
+ * @Date: 2022-10-09 21:59:06
+ * @LastEditors: xujie 1607526161@qq.com
+ * @FilePath: \HTML-CSS-Javascript-\dataStructure\index.js
+ * @Description: 
+ */
+console.log("dddd")
---
+++ dataStructure/test.ts
@@ -0,0 +1,10 @@
+/*
+ * @Author: xujie 1607526161@qq.com
+ * @Date: 2022-10-09 22:18:27
+ * @LastEditors: xujie 1607526161@qq.com
+ * @FilePath: \HTML-CSS-Javascript-\dataStructure\test.ts
+ * @Description: 
+ */
+enum Color { Red = 1, Green = 5, Blue = 3 };
+let c: Color = Color.Green;
+console.log(c)
