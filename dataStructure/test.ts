9ee0bcd5b761d8bb43b0f485eb494bd7bbae213b
x09898 <coder_xujie@163.com>
2022-10-21 13:37:33 +0800

    CSS复习

--- dataStructure/test.ts
+++ dataStructure/test.ts
@@ -1,18 +1,17 @@
 /*
  * @Author: xujie 1607526161@qq.com
  * @Date: 2022-10-09 22:18:27
- * @LastEditors: xujie 1607526161@qq.com
+ * @LastEditors: x09898 coder_xujie@163.com
  * @FilePath: \HTML-CSS-Javascript-\dataStructure\test.ts
  * @Description: 
  */
-interface paramsTs{
+interface paramsTs {
   label?: string,
   name?: string
 }
 function test(params: paramsTs) {
   console.log(params.label)
 }
-
 test({
   name: "ddd",
   label: "xujie"
