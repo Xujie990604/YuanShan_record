830ee6c5a11a6aee91e982438700e1e0f1917880
xujie <1607526161@qq.com>
2022-11-02 19:09:19 +0800

    1

--- dataStructure/stack/stack.js
+++ dataStructure/stack/stack.js
@@ -1,7 +1,7 @@
 /*
  * @Author: x09898 coder_xujie@163.com
  * @Date: 2022-10-27 17:26:11
- * @LastEditors: x09898 coder_xujie@163.com
+ * @LastEditors: xujie 1607526161@qq.com
  * @FilePath: \HTML-CSS-Javascript-\dataStructure\stack\stack.js
  * @Description: 
  */
@@ -12,7 +12,7 @@
  * @FilePath: \HTML-CSS-Javascript-\dataStructure\stack\stack.js
  * @Description: 定义一个栈的数据类型
  */
-class stack {
+class Stack {
   // 类的默认方法
   constructor() {
     this.item = []
@@ -48,3 +48,5 @@ class stack {
     this.item = []
   }
 }
+
+const stack = new Stack()
