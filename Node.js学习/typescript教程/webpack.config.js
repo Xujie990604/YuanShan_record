45f22442e8798457d3aa5b8ec0df95a5229f26b3
x09898 <coder_xujie@163.com>
2022-12-09 09:31:26 +0800

    1

--- Node.js学习/typescript教程/webpack.config.js
+++ Node.js学习/typescript教程/webpack.config.js
@@ -1,23 +1,23 @@
 /*
  * @Author: xujie 1607526161@qq.com
  * @Date: 2022-12-04 21:09:27
- * @LastEditors: xujie 1607526161@qq.com
+ * @LastEditors: x09898 coder_xujie@163.com
  * @FilePath: \HTML-CSS-Javascript-\Node.js学习\typescript教程\webpack.config.js
  * @Description: webpack的配置文件
  */
-const path = require("path")
+const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  mode: "development",
-  entry: "./src/test.ts",
+  mode: 'development',
+  entry: './src/test.ts',
   output: {
-    path: path.resolve(__dirname, "./dist"),
-    filename: "bundle.js"
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js'
   },
   resolve: {
-    extensions: [".ts", ".js", ".cjs", ".json"]
-  }, 
+    extensions: ['.ts', '.js', '.cjs', '.json']
+  },
   devServer: {
 
   },
@@ -25,13 +25,13 @@ module.exports = {
     rules: [
       {
         test: /\.ts$/,
-        loader: "ts-loader"
+        loader: 'ts-loader'
       }
     ]
   },
   plugins: [
-      new HtmlWebpackPlugin({
-        template: "./index.html"
-      })
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    })
   ]
 }
