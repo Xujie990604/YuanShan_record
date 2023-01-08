f2cd6dd5837033e2542cbbee48abdb5a2b875913
xujie <1607526161@qq.com>
2023-01-08 18:35:21 +0800

    （java）java基础概念

---
+++ Java/Employee.java
@@ -0,0 +1,45 @@
+/*
+ * @Author: xujie 1607526161@qq.com
+ * @Date: 2023-01-07 18:09:36
+ * @LastEditors: xujie 1607526161@qq.com
+ * @FilePath: \HTML-CSS-Javascript-\Java\Employee.java
+ * @Description: 
+ */
+
+// import java.io.*;
+
+public class Employee {
+  String name;
+  int age;
+  String designation;
+  double salary;
+
+  // Employee 类的构造器
+  public Employee(String name) {
+    this.name = name;
+  }
+
+  // 设置age的值
+  public void empAge(int empAge) {
+    age = empAge;
+  }
+
+  /* 设置designation的值 */
+  public void empDesignation(String empDesig) {
+    designation = empDesig;
+  }
+
+  /* 设置salary的值 */
+  public void empSalary(double empSalary) {
+    salary = empSalary;
+  }
+
+  /* 打印信息 */
+  public void printEmployee() {
+    System.out.println("名字:" + name);
+    System.out.println("年龄:" + age);
+    System.out.println("职位:" + designation);
+    System.out.println("薪水:" + salary);
+  }
+}
+
---
+++ Java/EmployeeTest.java
@@ -0,0 +1,17 @@
+/*
+ * @Author: xujie 1607526161@qq.com
+ * @Date: 2023-01-07 18:28:17
+ * @LastEditors: xujie 1607526161@qq.com
+ * @FilePath: \HTML-CSS-Javascript-\Java\EmployeeTest.java
+ * @Description: 
+ */
+// import java.io.*;
+
+public class EmployeeTest {
+
+  public static void main(String[] args) {
+    int name;
+    name = 122;
+    System.out.println(name);
+  }
+}
