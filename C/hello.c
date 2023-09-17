e31404551e487db2b408d4148809cfa23a302255
coder_xujie <coder_xujie@163.com>
2023-09-17 13:43:36 +0800

    09-17

--- C/hello.c
+++ C/hello.c
@@ -1,8 +1,25 @@
+// talkback.c -- 演示与用户交互
 #include <stdio.h>
-    int main(void)
+#include <string.h>  // 提供strlen()函数的原型
+#define DENSITY 62.4 // 人体密度（单位：磅/立方英尺）
+int main()
 {
-  char a = "dddd";
-  printf("/a");
+  float weight, volume;
+  int size, letters;
+  char name[40]; // name是一个可容纳40个字符的数组
+
+  printf("Hi! What's your first name?\n");
+  scanf("%s", name);
+  printf("%s, what's your weight in pounds?\n", name);
+  scanf("%f", &weight);
+  size = sizeof(name);
+  letters = strlen(name);
+  volume = weight / DENSITY;
+  printf("Well, %s, your volume is %2.2f cubic feet.\n",
+         name, volume);
+  printf("Also, your first name has %d letters,\n",
+         letters);
+  printf("and we have %d bytes to store it.\n", size);
 
   return 0;
 }
